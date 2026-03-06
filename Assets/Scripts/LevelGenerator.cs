28956afb304667ef41edaf7cd5a27b01772d8f3f
anhtuan170205 <tuan.tran170205@hcmut.edu.vn>
2026-03-06 09:53:40 +0700

    Move Chunk

--- Assets/Scripts/LevelGenerator.cs
+++ Assets/Scripts/LevelGenerator.cs
@@ -6,13 +6,37 @@ public class LevelGenerator : MonoBehaviour
     [SerializeField] private int _initialChunks = 5;
     [SerializeField] private Transform _chunkParent;
     [SerializeField] private float _chunkLength = 10f;
+    [SerializeField] private float _moveSpeed = 5f;
+
+    private GameObject[] _activeChunks = new GameObject[5];
 
     private void Start()
     {
+        SpawnChunks();
+    }
+
+    private void SpawnChunks()
+    {
         for (int i = 0; i < _initialChunks; i++)
         {
-            Instantiate(_chunkPrefab, new Vector3(0, 0, i * _chunkLength), Quaternion.identity, _chunkParent);
+            Vector3 spawnPosition = new Vector3(0, 0, i * _chunkLength);
+            GameObject newChunk =Instantiate(_chunkPrefab, spawnPosition, Quaternion.identity, _chunkParent);
+            _activeChunks[i] = newChunk;
         }
-
     }
+
+    private void Update()
+    {
+        MoveChunks();
+    }
+
+    private void MoveChunks()
+    {
+        if (_activeChunks == null) return;
+        for (int i = 0; i < _activeChunks.Length; i++)
+        {
+            _activeChunks[i].transform.Translate(-transform.forward * _moveSpeed * Time.deltaTime);
+        }
+    }
+
 }
