6610e5466d4d3c53d6de2f5e4528bdef3f5f47f1
anhtuan170205 <tuan.tran170205@hcmut.edu.vn>
2026-03-09 18:52:59 +0700

    Spawn Obstacles

---
+++ Assets/Scripts/ObstacleSpawner.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+using System.Collections;
+
+public class ObstacleSpawner : MonoBehaviour
+{
+    [SerializeField] private GameObject _obstaclePrefab;
+    [SerializeField] private float _spawnInterval = 2f;
+
+    private int _spawnedObstaclesCount = 0;
+
+    private void Start()
+    {
+        StartCoroutine(SpawnObstacles());
+    }
+
+    private IEnumerator SpawnObstacles()
+    {
+        while (_spawnedObstaclesCount < 10)
+        {
+            SpawnObstacle();
+            yield return new WaitForSeconds(_spawnInterval);
+        }
+    }
+
+    private void SpawnObstacle()
+    {
+        Vector3 spawnPosition = new Vector3(Random.Range(-3f, 3f), 5, transform.position.z);
+        Instantiate(_obstaclePrefab, spawnPosition, Quaternion.identity);
+        _spawnedObstaclesCount++;
+    }
+}
